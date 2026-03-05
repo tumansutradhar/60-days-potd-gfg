3b3e436e66b02fc3cbd8c0f17fb53b2c6c152767
tumansutradhar <officialbishalsutradhar@gmail.com>
2026-03-05 12:58:33 +0530

    feat: Add GFG POTD solution for March 5, 2025

---
+++ 2025-03-05_LongestSubstringWithKUniques.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    int longestKSubstr(string &s, int k)
+    {
+        unordered_map<char, int> mp;
+        int left = 0, maxLen = -1;
+
+        for (int right = 0; right < s.size(); right++)
+        {
+            mp[s[right]]++;
+
+            while (mp.size() > k)
+            {
+                mp[s[left]]--;
+                if (mp[s[left]] == 0)
+                    mp.erase(s[left]);
+                left++;
+            }
+
+            if (mp.size() == k)
+                maxLen = max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+};
